29e86cdd81cdba001dd6085673642ef0be55d2ae
Quincy25102017 <kunwar25102017@gmail.com>
2024-12-20 11:31:16 +0100

    1st com

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void Hello(int quincy)
+{
+	if (quincy == 1)
+		printf("Hello world\n");
+	else
+		printf("Hello papa\n");
+
+}
+
+
+
+int main()
+{
+
+	Hello(2);
+	return 0;
+	
+}
